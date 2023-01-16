d50dac43ed5a35f07cc970350af52a2670f89db3
shep-js <kateshakurina@gmail.com>
2023-01-16 10:49:46 +0100

    feat: implement towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix == null || matrix.length === 0) {
+        return [];
+    }
+    let newMatrix = [];
+    for (i = 0; i < matrix.length; i += 2) {
+        newMatrix.push(matrix[i]);
+        if (matrix[i + 1] === undefined) {
+            break;
+        } else {
+            newMatrix.push(matrix[i + 1].reverse());
+        }
+    }
+    let answer = [];
+    for (a = 0; a < newMatrix.length; a++) {
+        let arrMatrix = newMatrix[a];
+        for (b = 0; b < arrMatrix.length; b++) {
+            answer.push(arrMatrix[b]);
+        }
+    }
+    return answer;
+};
